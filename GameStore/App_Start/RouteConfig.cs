5762c816c73a331c13991bf3938fede670d8987b
Kateryna Antonova <Kateryna_Antonova@epam.com>
2018-04-27 17:39:23 +0300

    task 2 commit for CI

--- GameStore/App_Start/RouteConfig.cs
+++ GameStore/App_Start/RouteConfig.cs
@@ -8,7 +8,6 @@ namespace GameStore
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            //routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
                 name: "games",
