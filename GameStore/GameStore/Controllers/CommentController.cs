3fcf93659f93e82ce844c008ec39dc6a9cf195d7
Kateryna Antonova <Kateryna_Antonova@epam.com>
2018-04-30 14:37:05 +0300

    task 2 fix commentsController

--- GameStore/GameStore/Controllers/CommentController.cs
+++ GameStore/GameStore/Controllers/CommentController.cs
@@ -62,7 +62,7 @@ namespace GameStore.Controllers
                 comment.ParentCommentId = parentsCommentId.Value;
             }
 
-            if (quote != null && quote != string.Empty)
+            if (!string.IsNullOrEmpty(quote))
             {
                 comment.Quote = quote;
             }
