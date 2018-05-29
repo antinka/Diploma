bbb6541fbd8625ff4fa90e7a4f082b6728b61d80
Kateryna Antonova <Kateryna_Antonova@epam.com>
2018-05-29 18:22:56 +0300

    task 4 upd BsonSerializer, view order

--- GameStore/GameStore.Web/ViewModels/OrderViewModel.cs
+++ GameStore/GameStore.Web/ViewModels/OrderViewModel.cs
@@ -18,20 +18,28 @@ namespace GameStore.Web.ViewModels
         [Display(Name = "Shipper")]
         public string ShipperId { get; set; }
 
+        [Display(Name = "Ship Via")]
         public int? ShipVia { get; set; }
 
+        [Display(Name = "Freight")]
         public decimal? Freight { get; set; }
 
+        [Display(Name = "Ship Name")]
         public string ShipName { get; set; }
 
+        [Display(Name = "Ship Address")]
         public string ShipAddress { get; set; }
 
+        [Display(Name = "Ship City")]
         public string ShipCity { get; set; }
 
+        [Display(Name = "Ship Region")]
         public string ShipRegion { get; set; }
 
+        [Display(Name = "Ship Postal Code")]
         public string ShipPostalCode { get; set; }
 
+        [Display(Name = "Ship Country")]
         public string ShipCountry { get; set; }
 
         public SelectList ShipperList { get; set; }
