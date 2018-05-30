5b1f0c6edcfc0a66c358d7633e5fa96f46c5db3b
Kateryna Antonova <Kateryna_Antonova@epam.com>
2018-05-30 15:12:10 +0300

    task 3 AutomaticMigrationsEnable

--- GameStore/GameStore.DAL/Migrations/Configuration.cs
+++ GameStore/GameStore.DAL/Migrations/Configuration.cs
@@ -11,7 +11,7 @@ namespace GameStore.DAL.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(GameStore.DAL.EF.GameStoreDBContext context)
